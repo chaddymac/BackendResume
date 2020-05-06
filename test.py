97bd61a9c4bbd2bfdce7d2d19cb5ba3ef12ae259
Cha'Diamond Moody <moody@macbook-air.home>
2020-05-06 16:34:28 -0400

    switched to pytest

--- test.py
+++ test.py
@@ -1,10 +1,27 @@
+import pytest
 import unittest
 from Visitorcountread import lambda_handler
 import json
       
-class TestVisitor(unittest.TestCase):
-    def test_lambda_handler(self):
-        res_api = lambda_handler('a', 'b')
-        self.assertEqual(res_api['statusCode'],200,msg=None)
+# class TestVisitor(unittest.TestCase):
+#     def test_lambda_handler(self):
+#         res_api = lambda_handler('a', 'b')
+#         self.assertEqual(res_api['statusCode'],200,msg=None)
         
     #need add to pipeline python3 -m unittest test.py
+
+
+def test_lambda_handler(self):
+    res_api = lambda_handler('a', 'b')
+    self.assertEqual(res_api['statusCode'],200,msg=None)
+
+# def test_lambda_handler(apigw_event, mocker):
+
+#     ret = Visitorcountread.lambda_handler(apigw_event, "")
+#     data = json.loads(ret["body"])
+
+#     assert ret["statusCode"] == 200
+
+
+
+    # assert "location" in data.dict_keys()
