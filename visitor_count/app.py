5d739516f840d99ae65667f140b8a64acd805c1f
Cha'Diamond Moody <moody@MacBook-Air.local>
2020-05-26 00:23:06 -0400

    update added dynamoDB sam deploy

--- visitor_count/app.py
+++ visitor_count/app.py
@@ -3,23 +3,27 @@ import boto3
 
 
 def lambda_handler(event, context):
-#calling dynamondb
+    #calling dynamondb
     print("Change")
     dynamodb = boto3.client('dynamodb')
-#getting the dictionary for the Visitorcount table
+    #getting the dictionary for the Visitorcount table
     response=dynamodb.get_item(
-        TableName='Visitorcount', 
-        Key={'id':{'S':"item"}})
-#getting the current number for visitor count
-    count = response["Item"]["count"]['N']
-#adding one each time the site is hit
-    count = int(count) + 1
-#update the dynamodb table with the new number
-    # dynamodb.put_item(TableName='Visitorcount', 
-    #     Key={'id':{'S':"item"}}
+        TableName='htmlresume-Visitorcount-1SODZD7NQZA44', 
+         Key={'id':{'S':"item"}})
+    print(response)
     
-    dynamodb.put_item(
-        TableName='Visitorcount',
+    if "Item" not in response:
+        dynamodb.put_item(TableName='htmlresume-Visitorcount-1SODZD7NQZA44', 
+         Item={'id':{'S':'item',},'count':{'N':'0',}})
+    else:
+        #getting the current number for visitor count
+        count = response["Item"]["count"]['N']
+        #adding one each time the site is hit
+        count = int(count) + 1
+        #update the dynamodb table with the new number
+   
+        dynamodb.put_item(
+            TableName='htmlresume-Visitorcount-1SODZD7NQZA44',
         Item= {
         'id': {
             'S': 'item',
@@ -42,7 +46,7 @@ def lambda_handler(event, context):
        
     }
 
-
+# {"visitor":str(count)}
 
 if __name__ == '__main__':
    lambda_handler('a','b')
